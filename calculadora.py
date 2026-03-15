044b53d12af535b5d82ca41cbc0057e49b6f7d73
Maxuel Vanderlei <maxuelvanderlei0@gmail.com>
2026-03-14 21:45:37 -0300

    Adiciona POO na calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,30 +1,38 @@
-def somar(a, b):
+
+class Calculadora:
+
+    def somar(self, a, b):
         return f"{a} + {b} = {a+b}"
     
-def subtrair(a, b):
-    return f"{a} - {b} = {a-b}"
+    def subtrair(self, a, b):
+        return f"{a} - {b} = {a-b}"
     
-def multiplicar(a, b):
-    return f"{a} * {b} = {a*b}"
+    def multiplicar(self, a, b):
+        return f"{a} x {b} = {a*b}"
     
-def dividir(a, b):
-    if b != 0:
-        return f"{a} / {b} = {a/b}"
-    else:
-        return "Não é possível dividir por zero!"
+    def dividir(self, a, b):
+        if b != 0:
+            return f"{a} / {b} = {a/b}"
+        else:
+            mensagem = "Não é possível dividir por zero!"
+            return mensagem
+        
+    def mostrar_menu(self):
+        print("=" * 30)
+        print("=== C A L C U L A D O R A ====")
+        print("=" * 30)
+        print("1 - Somar")
+        print("2 - Subtrair")
+        print("3 - Multiplicar")
+        print("4 - Dividir")
+        print("0 - Sair")
+        print("=" * 30)
 
+calc = Calculadora()
 
 while True:
 
-    print("- - - - - - - - - - - - - - - - -")
-    print("- - - C A L C U L A D O R A - - -")
-    print("- - - - - - - - - - - - - - - - -")
-    print("1 - Somar")
-    print("2 - Subtrair")
-    print("3 - Multiplicar")
-    print("4 - Dividir")
-    print("0 - Sair")
-    print("- - - - - - - - - - - - - - - - - ")
+    calc.mostrar_menu()
 
     operacao = int(input("Escolha uma das opções acima: "))
 
@@ -39,15 +47,16 @@ while True:
     n2 = float(input("Digite o segundo número: "))
 
     if operacao == 1:
-        print(somar(n1, n2))
+        print(calc.somar(n1, n2))
     elif operacao == 2:
-        print(subtrair(n1, n2))
+        print(calc.subtrair(n1, n2))
     elif operacao == 3:
-        print(multiplicar(n1, n2))
+        print(calc.multiplicar(n1, n2))
     elif operacao == 4:
-        print(dividir(n1, n2))
-        
+        print(calc.dividir(n1, n2))
+            
 
 print("=" * 30)
 print("Obrigado por usar a calculadora!  :)")
 print("=" * 30)
+
